f52dac3d11c75e3f3cbed74edd2bb664870517f1
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-09-02 23:13:02 +0900

    ログイン、ログアウトの実装

--- blog-with-react-and-firebase/src/components/Login.js
+++ blog-with-react-and-firebase/src/components/Login.js
@@ -1,7 +1,24 @@
+import { signInWithPopup } from "firebase/auth";
 import React from "react";
+import { auth, provider } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWithGoole = () => {
+    //Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoole}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;
