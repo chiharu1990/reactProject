bba04dc534187a58c1f039331ec4b82e455ec469
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-09-08 00:08:32 +0900

    記事投稿ページの作成

--- blog-with-react-and-firebase/src/components/CreatePost.js
+++ blog-with-react-and-firebase/src/components/CreatePost.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./createPost.css";
 
-const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを入力" onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を入力" onChange={(e) => setPostText(e.target.value)} />
+        </div>
+        <button className="postButton" onClick={createPost}>
+          投稿する
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
