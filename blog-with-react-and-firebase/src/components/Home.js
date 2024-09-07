87cccf804f56dd97d75facb9764548b6a1b42f4e
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-09-08 00:11:43 +0900

    Homeページの作成

--- blog-with-react-and-firebase/src/components/Home.js
+++ blog-with-react-and-firebase/src/components/Home.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return <div>Home</div>;
+  return (
+    <div className="homePage">
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              {post.author.id === auth.currentUser?.uid && (
+                <button
+                  onClick={() => {
+                    handleDelete(post.id);
+                  }}
+                >
+                  削除
+                </button>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
 };
 
 export default Home;
